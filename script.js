a472528aa14f4301db9ec3de0cc568f19d73aa41
John-samuel-web <mokkaralajohnsamuel@gmail.com>
2024-07-29 15:38:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// // scripts.js
+// document.querySelectorAll('header nav ul li a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+// // Example of adding interactivity to the contact form
+// document.getElementById('contact').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     // Add validation logic here
+// });
